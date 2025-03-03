48a2529d76f0c7e4a32a5867be8453963a32b8cb
venkatanikesh <nikki@gmail.com>
2025-03-04 00:16:22 +0530

    Post Hello Controller

--- src/main/java/com/nikesh/springboot/controller/HelloController.java
+++ src/main/java/com/nikesh/springboot/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.nikesh.springboot.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +13,9 @@ public class HelloController {
         return "Hello World";
     }
 
-    @PostMapping("/hello")
-    public String helloPost() {
-        return "Hello World";
+    @PostMapping("/hello/{name}")
+    public String helloPost(@PathVariable String name) {
+        return "Hello " + name;
     }
+
 }
